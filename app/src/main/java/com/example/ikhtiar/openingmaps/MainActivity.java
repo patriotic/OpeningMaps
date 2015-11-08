033744972d464106e693c460c8fe9f88ad2ef8c3
Syed Ikhtiar Ahmed <ikhtiar0240@gmail.com>
2015-11-09 02:48:57 +0600

    Initial commit

---
+++ app/src/main/java/com/example/ikhtiar/openingmaps/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.ikhtiar.openingmaps;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by Ikhtiar on 11/9/2015.
+ */
+public class MainActivity extends AppCompatActivity {
+    private Button sendButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        sendButton = (Button) findViewById(R.id.send);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                send_notification();
+            }
+        });
+    }
+
+    private void send_notification() {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        Intent notifyIntent = new Intent(this, MapsActivity.class);
+        notifyIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        notifyIntent.putExtra("Lattitude", 24.918155);
+        notifyIntent.putExtra("Langitude", 91.830885);
+        PendingIntent notifyPendingIntent =
+                PendingIntent.getActivity(
+                        this,
+                        0,
+                        notifyIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        builder.setSmallIcon(R.drawable.ic_launcher);
+        builder.setContentTitle("HELP");
+        builder.setContentText("SOME ONE NEED HELP!!TAP TO VIEW LOCATION.");
+        builder.setContentIntent(notifyPendingIntent);
+        builder.setAutoCancel(true);
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(1000, builder.build());
+    }
+}
